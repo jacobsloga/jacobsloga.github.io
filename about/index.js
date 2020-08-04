68aa5c3fbc99d5645d59d8bfe89743b9d6ac662a
jacobsloga <jacobsloga4@gmail.com>
2020-08-04 22:38:24 +0100

    Swahili language added

--- about/index.js
+++ about/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 var is_swahili = false;
 
 // ON PAGE LOAD
@@ -11,10 +10,8 @@ async function start() {
 
 function toggle_language() {
   if (is_swahili) {
-    console.log("To English");
     to_english();
   } else {
-    console.log("To Swahili");
     to_swahili();
   };
 
@@ -22,21 +19,18 @@ function toggle_language() {
   return;
 }
 
-
 async function to_english() {
   const content_dict = await load_json("https://raw.githubusercontent.com/jacobsloga/jacobsloga.github.io/master/about/assets/language/english.json");
   if (content_dict == "") {return;};
-  console.log(content_dict);
-
-  document.getElementById("language").src = "/assets/languages/english.png";
+  sub_content(content_dict);
+  document.getElementById("language").src = "/assets/languages/swahili.png";
 }
 
 async function to_swahili() {
   const content_dict = await load_json("https://raw.githubusercontent.com/jacobsloga/jacobsloga.github.io/master/about/assets/language/swahili.json");
   if (content_dict == "") {return;};
-  console.log(content_dict);
-
-  document.getElementById("language").src = "/assets/languages/swahili.png";
+  sub_content(content_dict);
+  document.getElementById("language").src = "/assets/languages/english.png";
 }
 
 // Read content from file
@@ -45,10 +39,28 @@ async function load_json(file_url) {
 
   await fetch(file_url)
   .then(res => res.json())
-  .then(data => function (){
-    console.log(data);
+  .then(data => {
     facts = data;
   });
 
   return facts;
+}
+
+// Sub content in html
+function sub_content(content_dict) {
+  document.getElementById("nav-1").innerHTML = content_dict.navbar[0];
+  document.getElementById("nav-2").innerHTML = content_dict.navbar[1];
+  document.getElementById("nav-3").innerHTML = content_dict.navbar[2];
+
+  document.getElementById("about-heading").innerHTML = content_dict.about.heading;
+  document.getElementById("about-subheading").innerHTML = content_dict.about.subheading;
+  document.getElementById("about-text-1").innerHTML = content_dict.about.text_body[0];
+  document.getElementById("about-text-2").innerHTML = content_dict.about.text_body[1];
+  document.getElementById("about-text-3").innerHTML = content_dict.about.text_body[2];
+
+  document.getElementById("future-heading").innerHTML = content_dict.future.heading;
+  document.getElementById("future-subheading").innerHTML = content_dict.future.subheading;
+  document.getElementById("future-text-1").innerHTML = content_dict.future.text_body[0];
+  document.getElementById("future-text-2").innerHTML = content_dict.future.text_body[1];
+  document.getElementById("future-text-3").innerHTML = content_dict.future.text_body[2];
 }
